970f3ec28ba2df6d9c8544615a8cd1a22429b370
massmr <marcellin.massimo@gmail.com>
2024-01-09 06:36:49 +0100

    1st use of props

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, 
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
 
 const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    }, 
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ];
+
   return (
     <div>
       <h1>Hacker Stories</h1>
       <Search />
       <hr />
-      <List /> 
+      <List list={stories}/> 
     </div>
   );
 };
 
-const List = () => (
+//use of props as argument
+const List = (props) => (
   <ul>
-    {list.map((item) => {
+    {props.list.map((item) => {
       return (
         <li key={item.objectID}>
           <span>{item.title}</span>
